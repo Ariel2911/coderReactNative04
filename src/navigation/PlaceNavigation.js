1e7f114b931e48906e7ce13fc34fd6a96a6f55b2
Ariel Barrios <ariel29111978@hotmail.com>
2022-07-15 13:21:39 -0300

    Agrega un bóton para navegar hacia la pantalla 'Nueva dirreción'

--- src/navigation/PlaceNavigation.js
+++ src/navigation/PlaceNavigation.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Platform } from "react-native";
+import { Platform, TouchableOpacity } from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import PlaceDetailScreen from "../screens/PlaceDetailScreen";
@@ -7,6 +7,7 @@ import NewPlaceScreen from "../screens/NewPlaceScreen";
 import MapScreen from "../screens/MapSreen";
 import PlaceListScreen from "../screens/PlaceListScreen";
 import { colors } from '../utils/colors'
+import IonicIcons from '@expo/vector-icons/Ionicons';
 
 const Stack = createNativeStackNavigator();
 
@@ -27,9 +28,18 @@ const PlaceNavigator = () => (
       <Stack.Screen
         name="Place"
         component={PlaceListScreen}
-        options={{
+        options={({ navigation }) => ({
           title: 'Direcciones',
-        }}
+          headerRight: () => (
+            <TouchableOpacity onPress={() => navigation.navigate("NewPlace")} >
+              <IonicIcons 
+                name='add-circle-outline'
+                size={25}
+                color= {colors.black}
+              />
+            </TouchableOpacity>
+          )
+        })}
       />
       <Stack.Screen
         name="PlaceDetail"
@@ -43,6 +53,13 @@ const PlaceNavigator = () => (
         component={NewPlaceScreen}
         options={{
           title: 'Nueva dirección',
+        }}
+      />
+      <Stack.Screen
+        name="Map"
+        component={MapScreen}
+        options={{
+          title: 'Mapa',
         }}
       />
     </Stack.Navigator>
